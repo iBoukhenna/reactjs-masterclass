fcb55ac03e6dd3ab8244cb613ddaaa6e3dbea38e
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-27 11:07:36 +0100

    first Component

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
 const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container)
-let n = 0
 
-function numberFormat(n) {
-    return n.toString().padStart(2, '0')
+function Welcome(props) {
+    console.log(props)
+    return <h1>Hello {props.name}</h1>
 }
 
-// react update only the span, not like innerHTML
-function render(root) {
-    const items = [
-        'Task 1',
-        'Task 2',
-        'Task 3'
-    ]
-    const lis = items.map((item, k) => <li key={k}>{item}</li>)
-    // the attr class replaced by className 
-    const title = <React.Fragment>
-        <h1 className="title" id={"title" + n}>
-            Hello World ! <span>{['timer : ', n % 2 ? numberFormat(n) : null]}</span>
-        </h1>
-        <ul>
-            {lis}
-        </ul>
-    </React.Fragment>
-
-    root.render(title)
-}
-
-render(root)
-
-window.setInterval(() => {
-    n++
-    render(root)
-}, 1000)
+root.render(<Welcome name="Amine" />)
