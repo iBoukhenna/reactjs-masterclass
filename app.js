c480cab312afa9f0f3471c732d03d1c6ca3ff704
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-27 10:27:41 +0100

    add timer

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-function render() {
+const container = document.querySelector('#app')
+const root = ReactDOM.createRoot(container)
+let n = 0
+
+// react update only the span, not like innerHTML
+function render(root) {
     const title = React.createElement('h1', {}
         , 'Hello World ! '
-    );
+        , React.createElement('span', {}, n)
+    )
 
-    const container = document.querySelector('#app');
-    const root = ReactDOM.createRoot(container);
-    root.render(title);
+    root.render(title)
 }
 
-render();
+render(root)
+
+window.setInterval(() => {
+    n++
+    render(root)
+}, 1000)
