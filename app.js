fa137521618dbb4f8fce47c1ec8f9c307c0c3d62
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-27 11:17:17 +0100

    function vs class Component

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container)
 
-function Welcome({name, children}) {
+function WelcomeFunc({name, children}) {
     return <div>
         <h1>Hello {name}</h1>
         <p>{children}</p>
     </div>
+}
+
+class Welcome extends React.Component {
+
+    // super must be called
+    constructor (props) {
+        super(props)
+        console.log(props)
+    }
+
+    render() {
+        return <div>
+            <h1>Hello {this.props.name}</h1>
+            <p>{this.props.children}</p>
+        </div>
+    }
 }
 
 root.render(<Welcome name="Amine">Hello World !</Welcome>)
