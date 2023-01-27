1bc7eb2e554adb5c64cc7f29adf8650c26025f9d
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-27 11:11:13 +0100

    use Destructuring and Children

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container)
 
-function Welcome(props) {
-    console.log(props)
-    return <h1>Hello {props.name}</h1>
+function Welcome({name, children}) {
+    return <div>
+        <h1>Hello {name}</h1>
+        <p>{children}</p>
+    </div>
 }
 
-root.render(<Welcome name="Amine" />)
+root.render(<Welcome name="Amine">Hello World !</Welcome>)
