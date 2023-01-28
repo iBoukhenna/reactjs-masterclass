80c2cd017e8bbb5861b11b066eb2aba061b46211
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-28 16:50:42 +0100

    add css file

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -1,15 +1,29 @@
 import React, {Component} from 'react'
+import './Clock.css'
 
 export class Clock extends Component {
     constructor (props) {
         super(props)
         this.state = {
-            date : new Date()
+            date: new Date()
         }
+        this.timer = null
+    }
+
+    componentDidMount () {
+        this.timer = window.setInterval(() => {
+            this.setState({
+                date: new Date()
+            })
+        })
+    }
+
+    componentWillUnmount () {
+        window.clearInterval(this.timer)
     }
 
     render () {
-        return <div>
+        return <div className="clock">
             {this.state.date.toLocaleString()}
         </div>
     }
