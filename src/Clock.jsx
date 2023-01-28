7eb42e4d12b8c33e05ed26ea4b58d9c5e568d253
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-28 16:42:26 +0100

    add Component for new app react

---
+++ src/Clock.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+
+export class Clock extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            date : new Date()
+        }
+    }
+
+    render () {
+        return <div>
+            {this.state.date.toLocaleString()}
+        </div>
+    }
+}
