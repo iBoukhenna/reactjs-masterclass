1e4f0729fe060ba316ced2fe7247d0e38a2efe67
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-28 10:39:10 +0100

    update all component

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
 const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container)
 
-class Field extends React.Component {
+function Field ({name, value, onChange, children}) {
+    return <div className="form-group">
+        <label htmlFor={name}>{children} : </label>
+        <input type="text" id={name} name={name} value={value} onChange={onChange} className="form-control" />
+    </div>
+}
 
-    render () {
-        const {name, label, value, onChange} = this.props
-        return <div className="form-group">
-            <label htmlFor={name}>{label} : </label>
-            <input type="text" id={name} name={name} value={value} onChange={onChange} className="form-control" />
-        </div>
-    }
+function TextArea ({name, value, onChange, children}) {
+    return <div className="form-group">
+        <label htmlFor={name}>{children} : </label>
+        <textarea id={name} name={name} value={value} onChange={onChange} className="form-control"></textarea>
+    </div>
+}
 
+function CheckBox ({name, value, onChange, children}) {
+    return <div className="form-check">
+        <label htmlFor={name}>{children} : </label>
+        <input type="checkbox" id={name} name={name} checked={value} onChange={onChange} className="form-control" />
+    </div>
+}
+
+function MultiSelect ({name, value, items, onChange, children}) {
+    const options = items.map((item) => <option key={item.key} value={item.key}>{item.value}</option>)
+    return <div className="form-select">
+        <label htmlFor={name}>{children} : </label>
+        <select id={name} name={name} value={value} onChange={onChange} multiple className="form-control">
+            {options}
+        </select>
+    </div>
+}
+
+function Select ({name, value, items, onChange, children}) {
+    const options = items.map((item) => <option key={item.key} value={item.key}>{item.value}</option>)
+    return <div className="form-select">
+        <label htmlFor={name}>{children} : </label>
+        <select id={name} name={name} value={value} onChange={onChange} className="form-control">
+            {options}
+        </select>
+    </div>
+}
+
+function CheckBox ({name, value, onChange, children}) {
+    return <div className="form-check">
+        <label htmlFor={name}>{children} : </label>
+        <input type="checkbox" id={name} name={name} checked={value} onChange={onChange} className="form-control" />
+    </div>
 }
 
 class Home extends React.Component {
@@ -18,7 +54,8 @@ class Home extends React.Component {
     constructor (props) {
         super(props)
         this.state = {
-            name: 'Amine',
+            last_name: 'Amine',
+            first_name: 'Ahmed',
             address: 'Algiers',
             status: 'married',
             projects: ['P01', 'P03'],
@@ -38,42 +75,29 @@ class Home extends React.Component {
     }
 
     render () {
-        console.log('render')
-        return <div>
+        const statusItems = [{key:"single",value:"Single"},{key:"married",value:"Married"},{key:"divorce",value:"Divorce"},{key:"separated",value:"Separated"}]
+        const projectItems = [{key:"P01",value:"Project 01"},{key:"P02",value:"Project 02"},{key:"P03",value:"Project 03"}]
+        return <div className="container">
                 <div className="form-group">
                     <label htmlFor="title">Title : </label>
                     <input type="text" id="title" name="title" defaultValue="Engineer" />
                 </div><br/>
-                <Field name="name" label="Name" value={this.state.name} onChange={this.handleChange} />
+                <Field name="last_name" value={this.state.last_name} onChange={this.handleChange}>Last Name</Field>
                 <br/>
-                <div className="form-group">
-                    <label htmlFor="address">Address : </label>
-                    <textarea id="address" name="address" value={this.state.address} onChange={this.handleChange}></textarea>
-                </div><br/>
-                <div className="form-group">
-                    <label htmlFor="status">Status : </label>
-                    <select id="status" name="status" value={this.state.status} onChange={this.handleChange}>
-                        <option value="single">single</option>
-                        <option value="married">married</option>
-                        <option value="divorce">divorce</option>
-                        <option value="separated">separated</option>
-                    </select>
-                </div><br/>
-                <div className="form-group">
-                    <label htmlFor="projects">Projects : </label>
-                    <select id="projects" name="projects" value={this.state.projects} onChange={this.handleChange} multiple>
-                        <option value="P01">Project 01</option>
-                        <option value="P02">Project 02</option>
-                        <option value="P03">Project 03</option>
-                    </select>
-                </div><br/>
-                <div className="form-group">
-                    <label htmlFor="gendre">Gendre : </label>
-                    <input type="checkbox" id="gendre" name="gendre" value={this.state.gendre} onChange={this.handleChange} />
-                </div><br/>
+                <Field name="first_name" value={this.state.first_name} onChange={this.handleChange}>First Name</Field>
+                <br/>
+                <TextArea name="address" value={this.state.address} onChange={this.handleChange}>Address</TextArea>
+                <br/>
+                <Select name="status" items={statusItems} value={this.state.status} onChange={this.handleChange}>Status</Select>
+                <br/>
+                <MultiSelect name="projects" items={projectItems} value={this.state.projects} onChange={this.handleChange}>Projects</MultiSelect>
+                <br/>
+                <CheckBox name="gendre" value={this.state.gendre} onChange={this.handleChange}>Gendre</CheckBox>
+                <br/>
             <br/>
             <br/>{JSON.stringify(this.state)}
-            <br/>{this.state.name}
+            <br/>{this.state.last_name}
+            <br/>{this.state.first_name}
             <br/>{this.state.address}
             <br/>{this.state.status}
             <br/>{JSON.stringify(this.state.projects)}
