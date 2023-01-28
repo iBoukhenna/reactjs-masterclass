0a91c8e2df2b365e0d03e735c76b56ea6b3750f8
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-28 16:20:32 +0100

    forward all props

--- app.js
+++ app.js
@@ -2,13 +2,13 @@ const container = document.querySelector('#app')
 const root = ReactDOM.createRoot(container)
 
 const FieldWithRef = React.forwardRef((props, ref) => {
-    return <Field forwardRef={ref} />
+    return <Field forwardRef={ref}  {...props} />
 })
 
 class Field extends React.Component {
-
     render () {
         return <div className="form-group">
+            <label>{this.props.label}</label>
             <input type="text" className="form-control" ref={this.props.forwardRef}/>
         </div>
     }
@@ -28,7 +28,7 @@ class Home extends React.Component {
 
     render () {
         return <div>
-            <FieldWithRef ref={this.input}/>
+            <FieldWithRef ref={this.input} label="demo" />
             <button onClick={this.handleClick}>Test</button>
         </div>
     }
