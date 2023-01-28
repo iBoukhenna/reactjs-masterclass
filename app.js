364992ec9b0308ab73f4c295191fc41b87a0aef2
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-01-28 16:04:22 +0100

    refs with React createRef

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ class Home extends React.Component {
     constructor (props) {
         super(props)
         this.handleClick = this.handleClick.bind(this)
-        this.input = null
+        this.input = React.createRef()
     }
 
     handleClick (e) {
-        console.log(e, this.input.value)
+        console.log(this.input.current.value)
     }
 
     render () {
         return <div>
-            <input type="text" ref={(ref) => this.input = ref} />
+            <input type="text" ref={this.input} />
             <button onClick={this.handleClick}>Test</button>
         </div>
     }
