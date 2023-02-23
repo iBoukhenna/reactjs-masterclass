68ec1a288f79bec4664a910ee3d59431a03ff654
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-23 17:47:05 +0100

    simple use of useState

--- src/index.jsx
+++ src/index.jsx
@@ -1,13 +1,17 @@
-import {render} from 'react-dom'
-import React from 'react'
-import './index.css'
+import { createRoot } from 'react-dom/client'
+import React, { useState } from 'react'
 
-import {List} from './List.jsx'
+function Counter () {
+    const count = useState(0)
+    // it contains an array with the value and function to set the value
+    console.log(count)
+    return <button></button>
+}
 
-render(
+const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
+root.render(
     <div>
-        Hello world 
-        <List />
-    </div>,
-    document.getElementById("app")
+        <Counter />
+    </div>
 )
