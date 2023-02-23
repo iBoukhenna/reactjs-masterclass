11c577055a294075128873f6b896c0613ad65118
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-23 18:01:19 +0100

    create two hook with two component

--- src/index.jsx
+++ src/index.jsx
@@ -2,25 +2,27 @@ import { createRoot } from 'react-dom/client'
 import React, { useState } from 'react'
 
 function Counter () {
-    // distructur the contains
-    const [state, setState] = useState({
-        a: 1
-    })
+    const [count, setCount] = useState(0)
+    const [count2, setCount2] = useState(0)
     // react use the execution context to know which instance of component to be update 
     const handleClick = function (e) {
         e.preventDefault()
-        setState(state => {
-            return {...state, count: 10}
-        })
+        setCount(c => c + 1)
     }
-    return <div onClick={handleClick}>{JSON.stringify(state)}</div>
+    const handleClick2 = function (e) {
+        e.preventDefault()
+        setCount2(c => c + 2)
+    }
+    return <>
+        <button onClick={handleClick}>increase : {count}</button>
+        <button onClick={handleClick2}>increase : {count2}</button>
+    </>
 }
 
 const rootElement = document.getElementById("app");
 const root = createRoot(rootElement);
 root.render(
     <div>
-        <Counter />
         <Counter />
     </div>
 )
