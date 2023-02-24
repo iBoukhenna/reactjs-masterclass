e3cfe63d726cf6fac7ab2e8323ea6f1755b5489a
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-24 01:35:58 +0100

    TodoList Hook

--- src/index.jsx
+++ src/index.jsx
@@ -35,6 +35,32 @@ function useToggle (init = true) {
     return [value, toggle]
 }
 
+function TodoList () {
+    const [todos, setTodos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(function () {
+        (async function () {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10')
+            const responseData = await response.json()
+            if (response.ok) {
+                setTodos(responseData)
+            } else {
+                alert(JSON.stringify(responseData))
+            }
+            setLoading(false)
+        })()
+    }, [])
+
+    if (loading) {
+        return 'Chargement ...'
+    }
+
+    return <ul>
+        {todos.map(t => <li key={t.title}>{t.title}</li>)}
+    </ul>
+}
+
 function Counter () {
     const count = useAutoIncrease(10)
 
@@ -50,6 +76,7 @@ function App () {
         Show the counter <input type="checkbox" onChange={toggleCounter} checked={counterVisible} />
         <br />
         {counterVisible && <Counter />}
+        <TodoList />
     </div>
 }
 
