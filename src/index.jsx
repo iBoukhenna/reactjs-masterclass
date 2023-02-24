296bd8ec9ff883812a84ba98d7e68c175ec96369
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-24 11:15:17 +0100

    new bugs for useMemo

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { useState, useMemo } from 'react'
+import React, { useState, useMemo } from 'react'
 
 function wait(duration) {
     const t = Date.now()
@@ -11,41 +11,21 @@ function wait(duration) {
 
 }
 
-function encode (number) {
-    wait(1000)
-    return Date.now()
-}
+const Button = React.memo (function ({onClick}) {
+    console.log('render')
+    return <button onClick={onClick}>My button</button>
+})
 
 function App () {
-    const [name, setName] = useState('Amine')
-    const [number, setNumber] = useState(0)
+    const [count, setCount] = useState(0)
 
-    const onChange = function (e) {
-        if (e.target.getAttribute('name') === 'name') {
-            setName(e.target.value)
-        }
-        if (e.target.name === 'number') {
-            setNumber(e.target.value)
-        }
+    const handleClick = function () {
+        alert('Hello')
     }
 
-    const encoded = useMemo(function () {
-        return encode(number)
-    }, [number])
-    // without useMemo when we change the name the function is called
-    // with useMemo when only the number change the function will be called
-    //const encoded = encode(number)
-
     return <div>
-        <div className="form-group">
-            <label htmlFor="name"></label>
-            <input name="name" id="name" type="text" value={name} onChange={onChange} />
-        </div>
-        <div className="form-group">
-            <label htmlFor="number"></label>
-            <input name="number" id="number" type="text" value={number} onChange={onChange} />
-        </div>
-        <h2>Encoded : {encoded}</h2>
+        <Button onClick={handleClick} />
+        <button onClick={() => setCount(c => c + 1)}>Increase</button>
     </div>
 }
 
