5910a02f40ba10fbc7b2b09906d2c87610e44290
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-24 11:39:53 +0100

    useRef : to save and get value with reference from DOM

--- src/index.jsx
+++ src/index.jsx
@@ -1,33 +1,17 @@
 import { createRoot } from 'react-dom/client'
-import React, { useState, useCallback } from 'react'
-
-function wait(duration) {
-    const t = Date.now()
-    while (true) {
-        if (Date.now() - t > duration) {
-            return true
-        }
-    }
-
-}
-
-const Button = React.memo(function ({onClick}) {
-    // this function will be called for each click because it's a new instance
-    console.log('render')
-    return <button onClick={onClick}>My button</button>
-})
+import React, { useState, useRef } from 'react'
 
 function App () {
-    const [count, setCount] = useState(0)
 
-    // replace useMemo by useCallback when the return is the function
-    const handleClick = useCallback(function () {
-            alert('Hello' + count)
-    }, [count])
+    const input = useRef(null)
+
+    const handleButtonClick = function () {
+        console.log(input.current.value)
+    }
 
     return <div>
-        <Button onClick={handleClick} />
-        <button onClick={() => setCount(c => c + 1)}>Increase</button>
+        <input type="text" ref={input} />
+        <button onClick={handleButtonClick}>Get value</button>
     </div>
 }
 
