1a713eccadb444741c3d1a965146c168d67057ab
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-02-24 16:29:00 +0100

    first useReducer

--- src/index.jsx
+++ src/index.jsx
@@ -1,36 +1,27 @@
 import { createRoot } from 'react-dom/client'
-import React, { useLayoutEffect, useCallback, useState, useRef } from 'react'
+import React, { useReducer } from 'react'
 
-function wait(duration) {
-    const t = Date.now()
-    while (true) {
-        if (Date.now() - t > duration) {
-            return true
-        }
+function reducer (state, action) {
+    switch (action.type) {
+        case 'increase':
+            return state + 1;
+        case 'decrease':
+            if (state <= 0) {
+                return state;
+            }
+            return state - 1;
+        default:
+            throw new Error("L'action " + action.type + " est inconnue")
     }
-
 }
 
 function App () {
-    const [count, setCount] = useState(0)
-    const button = useRef(null)
-
-    const increase = useCallback(() => {
-        setCount(c => c + 1)
-    }, [])
-
-    // useEffect is async but useLayoutEffect is sync
-    // useLayoutEffect for DOM manup otherwise use useEffect
-    useLayoutEffect(() => {
-        if (count % 2 === 0) {
-            button.current.style.color = 'red'
-        } else {
-            button.current.style.color = 'green'
-        }
-    }, [count])
+    const [count, dispatch] = useReducer(reducer, 0)
 
     return <div>
-        <button ref={button} onClick={increase}>Increase {count}</button>
+        Counter : {count}
+        <button onClick={() => dispatch({type: 'increase'})}>Increase</button>
+        <button onClick={() => dispatch({type: 'decrease'})}>Decrease</button>
     </div>
 }
 
