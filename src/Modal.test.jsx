ae3d3bceaae8c9c910940e760461d1c65376d2a3
DESKTOP-U469SNE\LENOVO X1 CARBON <boukhenna.ibrahim@gmail.com>
2023-03-02 17:05:01 +0100

    check with getByText using toBeInTheDocument

--- src/Modal.test.jsx
+++ src/Modal.test.jsx
@@ -1,14 +1,15 @@
-// yarn add -D @testing-library/react
-// yarn add -D @types/jest
-// yarn add -D babel-jest @babel/preset-env @babel/preset-react
+// yarn add -D @testing-library/dom
+// yarn add -D @testing-library/jest-dom
 
 import React from 'react';
 import { Modal } from './Modal';
 import { render } from '@testing-library/react';
+import { screen } from '@testing-library/dom';
+import '@testing-library/jest-dom/extend-expect';
 
 
 test('test Modal', function () {
-    render(<Modal title="Hello Every One !" onClose={() => null}><div id="demo"></div></Modal>)
-    const demo = document.querySelector('#demo')
-    expect(demo).not.toBeNull()
+    render(<Modal title="Hello Every One !" onClose={() => null}>Hello</Modal>)
+    const title = screen.getByText('Hello Every One !')
+    expect(title).toBeInTheDocument()
 })
